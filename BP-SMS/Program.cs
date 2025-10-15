fa1aa385014cfa21b49e2ba2104f54bb27c64f92
banpeter2007 <ban.peter@diak.szbi-pg.hu>
2025-10-16 00:36:29 +0200

    day_1
    creating_the_git_project

---
+++ BP-SMS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP_SMS
+{
+    internal class Program
+    {
+        #region fejlec
+        static void fejlec()
+        {
+            /*
+             BP - SMS
+             BP - 10.15.-10.17.
+            */
+
+            Type type = typeof(Program);
+            string namespaceName = type.Namespace;
+            Console.WriteLine(namespaceName);
+            for (int i = 0; i < namespaceName.Length; i++) Console.Write('-');
+            Console.WriteLine();
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            fejlec();
+
+            Console.WriteLine();
+            Console.WriteLine("Kilépéshez nyomja meg az ENTER billentyűt.");
+            Console.ReadLine();
+        }
+    }
+}
